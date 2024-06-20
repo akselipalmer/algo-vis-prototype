a420edea2c73477ec781733d8c31335deabc1fcd
Akseli Palmer <akseli@toddsden.com>
2024-06-20 19:07:54 -0300

    added dark light mode to Editor

--- app/editor/page.tsx
+++ app/editor/page.tsx
@@ -1,3 +1,18 @@
-export default function page() {
-  return <h1 className=" text-6xl">Code Editor</h1>;
+"use client";
+
+import { Editor } from "@monaco-editor/react";
+import { useTheme } from "next-themes";
+
+export default function CodeEditor() {
+  const { theme } = useTheme();
+
+  return (
+    <div className="h-full w-full">
+      <Editor
+        defaultLanguage="javascript"
+        defaultValue="// some comment"
+        theme={theme === "dark" ? "vs-dark" : "vs-light"}
+      />
+    </div>
+  );
 }
